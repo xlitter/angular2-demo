e6bef1886eb4abd048bdf0737984e7b58aa369d9
xlitter <9638527_4_1@163.com>
2015-12-29 02:04:56 +0800

    add table list

--- app/hero/components/app.component.ts
+++ app/hero/components/app.component.ts
@@ -11,13 +11,37 @@ interface Hero {
   selector: 'hero',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id:</label>{{hero.id}}</div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+      (click)="onSelect(hero)" 
+      [class.selected]="hero === selectHero">
+        <span>{{hero.id}}</span>{{hero.name}}
+      </li>
+    </ul>
+    <h2>{{selectHero.name}} details!</h2>
+    <div><label>id:</label>{{selectHero.id}}</div>
     <div>
       <label>name:</label>
-      <div><input [(ngModel)]="hero.name" placeholder="name"/></div>
+      <div><input [(ngModel)]="selectHero.name" placeholder="name"/></div>
     </div>
-   `
+   `,
+  styles: [`     
+    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+    .heroes .badge {
+      font-size: small;
+      color: white;
+      padding: 0.1em 0.7em;
+      background-color: #369;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -1px;
+    }
+    .selected { background-color: #EEE; color: #369; }
+  `]
 })
 export class AppComponent {
 
@@ -27,5 +51,25 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+
+  public heroes: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+  ];
+
+  public selectHero: Hero;
+
+  onSelect(hero) {
+    this.selectHero = hero;
+  }
+
 
 }
